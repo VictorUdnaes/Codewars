504939d6dfcc5bdc14478d2ed3936989769f452d
Victor Udnaes <udnaesv@gmail.com>
2021-12-27 17:43:20 +0100

    third commit

--- src/reverser.java
+++ src/reverser.java
@@ -1,5 +1,8 @@
 public class reverser {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println(num1 + num2);
     }
 }
